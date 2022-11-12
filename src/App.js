eefa059a1401ed17ee8af74519626065f171d79f
Shreedutt <shreedutt77@gmail.com>
2022-11-12 10:47:23 +0530

    Added theme to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import HomePage from "./pages/homePage";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#6F65EF",
+		},
+		secondary: {
+			// This is green.A700 as hex.
+			main: "##E8E8EF",
+		},
+	},
+});
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+		<div className='App'>
+			<HomePage />
+      </div>
+    </ThemeProvider>
+	);
 }
 
 export default App;
