ce677091a8277d3c18d316d0182c1fc4458b197a
Shreedutt <shreedutt77@gmail.com>
2022-11-13 15:35:43 +0530

    added subheader to home

--- src/layouts/home/Home.jsx
+++ src/layouts/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import SubHeader from '../../components/SubHeader/SubHeader'
+import Card from '../../components/Card/Card'
+import {
+    Container,
+} from "@mui/material";
 import "./Home.scss"
+import Data from "../../demo_data";
 
 function Home() {
     return (
-        <div>
+        <div >
             <Header />
+            <Container maxWidth='lg' disableGutters sx={{ marginTop: "36px" }}>
+                <SubHeader />
+            </Container>
         </div>
     )
 }
