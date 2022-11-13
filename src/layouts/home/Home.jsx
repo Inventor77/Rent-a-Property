c9c19cc9af2f714bd0a5245e3343d3b1c75974e8
Shreedutt <shreedutt77@gmail.com>
2022-11-13 20:44:01 +0530

    code refactoring

--- src/layouts/home/Home.jsx
+++ src/layouts/home/Home.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import SubHeader from '../../components/SubHeader/SubHeader'
+import React from "react";
+import Header from "../../components/Header/Header";
+import SubHeader from "../../components/SubHeader/SubHeader";
 import Filter from "../../components/Filter/Filter";
 import Card from "../../components/Card/Card";
 import { Container, Grid } from "@mui/material";
-import "./Home.scss"
-import Data from "../../demo_data";
+import "./Home.scss";
+import { useFilter } from "../../Context/FilterContext";
 
 function Home() {
-    return (
-			<div>
-				<Header />
-				<Container maxWidth='lg' disableGutters sx={{ marginTop: "36px" }}>
-					<SubHeader />
-					<Filter />
-					<Grid container className='card_container'>
-						{Data.map((data) => (
-							<Card key={data.id} data={data} />
-						))}
-					</Grid>
-				</Container>
-			</div>
-		);
+	const [filter, setFilter] = useFilter();
+
+	return (
+		<div>
+			<Header />
+			<Container maxWidth='lg' disableGutters sx={{ marginTop: "36px" }}>
+				<SubHeader />
+				<Filter />
+				<Grid container className='card_container'>
+					{filter?.map((data) => (
+						<Card key={data.id} data={data} />
+					))}
+				</Grid>
+			</Container>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
