04a8985ad0398e5b97a1a2f03e86ec9d61e98838
Shreedutt <shreedutt77@gmail.com>
2022-11-13 16:14:21 +0530

    Added all needed component to home

--- src/layouts/home/Home.jsx
+++ src/layouts/home/Home.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import SubHeader from '../../components/SubHeader/SubHeader'
-import Card from '../../components/Card/Card'
-import {
-    Container,
-} from "@mui/material";
+import Filter from "../../components/Filter/Filter";
+import Card from "../../components/Card/Card";
+import { Container, Grid } from "@mui/material";
 import "./Home.scss"
 import Data from "../../demo_data";
 
 function Home() {
     return (
-        <div >
-            <Header />
-            <Container maxWidth='lg' disableGutters sx={{ marginTop: "36px" }}>
-                <SubHeader />
-            </Container>
-        </div>
-    )
+			<div>
+				<Header />
+				<Container maxWidth='lg' disableGutters sx={{ marginTop: "36px" }}>
+					<SubHeader />
+					<Filter />
+					<Grid container className='card_container'>
+						{Data.map((data) => (
+							<Card data={data} />
+						))}
+					</Grid>
+				</Container>
+			</div>
+		);
 }
 
 export default Home
